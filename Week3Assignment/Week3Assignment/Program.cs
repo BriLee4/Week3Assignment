fb29cbd5ba81e74ac60b65927900d2459e65fc96
BriLee4 <brianlee5282000@gmail.com>
2022-02-12 01:30:29 -0600

    initial commit

---
+++ Week3Assignment/Week3Assignment/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Week3Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please enter the speed limit.");
+            string speedL = Console.ReadLine();
+            int speedLimit = int.Parse(speedL);
+            Car c = new Car();
+            Console.WriteLine("Please enter the make of your car.");
+            c.make = Console.ReadLine();
+            Console.WriteLine("Please enter the model of your car.");
+            c.model = Console.ReadLine();
+            Console.WriteLine("Please enter how fast you were going in MPH.");
+            c.mph = Console.ReadLine();
+            int carSpeed = int.Parse(c.mph);
+            
+            if (carSpeed > speedLimit)
+            {
+               
+
+                int demeritPoints = 0;
+                while (carSpeed < speedLimit)
+                {
+                    carSpeed -= 5;
+                    demeritPoints++;
+                }
+                if(demeritPoints > 10)
+                {
+                    Console.WriteLine("<LICENCE SUSPENDED>");
+                }
+                }
+                 else
+                {   
+                    Console.WriteLine("OK");
+                }
+           
+
+        }
+           
+    }
+        
+        public class Car
+        {
+ 
+            public string make;
+            public string model;
+            public string mph;
+
+                
+        }
+    
+}
